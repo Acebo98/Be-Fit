387a1320acd86ec986044751cffd2961255d44af
sergio <sergio98.am30@gmail.com>
2019-04-09 16:13:57 +0200

    clase para diálogos de confirmación

---
+++ app/src/main/java/com/example/befit/DialogoConfirmacion.java
@@ -0,0 +1,58 @@
+package com.example.befit;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class DialogoConfirmacion extends DialogFragment {
+
+    private MiDialogListener listener;
+
+    //Interfaz
+    public interface MiDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(getActivity());
+
+        //Datos del diálogo
+        Bundle bundle = getArguments();
+        String titulo = bundle.getString("TITULO");
+        String mensaje = bundle.getString("MENSAJE");
+
+        //Cuerpo del cuadro de diálogo
+        builder.setMessage(mensaje)
+                .setTitle(titulo)
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDialogPositiveClick(DialogoConfirmacion.this);
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();        //No hacer nada
+                }
+                });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Activity act = (Activity) context;
+
+        try {
+            listener = (MiDialogListener) act;
+        }
+        catch (ClassCastException e) {
+            throw new ClassCastException(act.toString() + "debes implementar DialogListener");
+        }
+    }
+}
