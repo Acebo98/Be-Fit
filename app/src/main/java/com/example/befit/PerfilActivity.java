8249fee20171f3c19a636bfef5f097bfb873c250
sergio <sergio98.am30@gmail.com>
2019-04-10 10:43:40 +0200

    wip en el modificado

--- app/src/main/java/com/example/befit/PerfilActivity.java
+++ app/src/main/java/com/example/befit/PerfilActivity.java
@@ -2,14 +2,17 @@ package com.example.befit;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class PerfilActivity extends AppCompatActivity {
+public class PerfilActivity extends AppCompatActivity implements View.OnClickListener, DialogoConfirmacion.MiDialogListener {
 
     EditText tbNombre;
     EditText tbContra;
@@ -17,6 +20,8 @@ public class PerfilActivity extends AppCompatActivity {
     Button btnBorrar;
     TextView labModificacion;
     TextView labEntrenamientos;
+
+    final String MODIF = "modif";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,10 @@ public class PerfilActivity extends AppCompatActivity {
         btnModificar = (Button)findViewById(R.id.btnModificar);
         labEntrenamientos = (TextView)findViewById(R.id.labNEntrenamientos);
         labModificacion = (TextView)findViewById(R.id.labUpdate);
+
+        //Eventos
+        btnModificar.setOnClickListener(this);
+        btnBorrar.setOnClickListener(this);
 
         //Leemos los datos del usuario
         ReadUserData();
@@ -70,6 +79,52 @@ public class PerfilActivity extends AppCompatActivity {
         }
         catch (Exception err) {
             LogeoActivity.centralizarToast(getApplicationContext(), err.getMessage());
+        }
+    }
+
+    //Comprobamos que sea correcto que el usuario se pueda registrar (Como en la actividad de logeo)
+    private boolean IsDataCorrect(String usuario, String contrasena) {
+        return usuario.length() >= 4 && contrasena.length() >= 4;
+    }
+
+    @Override
+    public void onClick(View v) {
+        try {
+            switch (v.getId()) {
+                case R.id.btnBorrar: {
+
+                }
+                break;
+                case R.id.btnModificar: {
+                    String usuario = tbNombre.getText().toString().trim();
+                    String contra = tbContra.getText().toString().trim();
+                    if (IsDataCorrect(usuario, contra) == true) {
+
+                        //Cuadro de diálogo
+                        DialogoConfirmacion dialogoConfirmacion = new DialogoConfirmacion();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("TITULO", "Modificación");
+                        bundle.putString("MENSAJE", "¿Estás seguro de que quieres modificar tus datos?");
+                        dialogoConfirmacion.setArguments(bundle);
+                        dialogoConfirmacion.show(getSupportFragmentManager(), MODIF);
+                    }
+                    else {
+                        LogeoActivity.centralizarToast(getApplicationContext(), "Recuerda de que el nombre de " +
+                                "usuario y la contraseña deben de tener 4 carácteres como mínimo");
+                    }
+                }
+                break;
+            }
+        }
+        catch (Exception err) {
+            LogeoActivity.centralizarToast(getApplicationContext(), err.getMessage());
+        }
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        if (dialog.getTag() == MODIF) {
+
         }
     }
 }
