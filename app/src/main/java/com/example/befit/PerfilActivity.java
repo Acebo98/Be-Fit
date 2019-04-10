3e29d718a92b987f15042ab0975010ac3ee36b83
sergio <sergio98.am30@gmail.com>
2019-04-10 10:30:05 +0200

    lectura de datos iniciales más cambios de algunas strings

--- app/src/main/java/com/example/befit/PerfilActivity.java
+++ app/src/main/java/com/example/befit/PerfilActivity.java
@@ -1,10 +1,22 @@
 package com.example.befit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PerfilActivity extends AppCompatActivity {
+
+    EditText tbNombre;
+    EditText tbContra;
+    Button btnModificar;
+    Button btnBorrar;
+    TextView labModificacion;
+    TextView labEntrenamientos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,16 @@ public class PerfilActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //IDS
+        tbContra = (EditText)findViewById(R.id.tbMiContra);
+        tbNombre = (EditText)findViewById(R.id.tbMiNombre);
+        btnBorrar = (Button)findViewById(R.id.btnBorrar);
+        btnModificar = (Button)findViewById(R.id.btnModificar);
+        labEntrenamientos = (TextView)findViewById(R.id.labNEntrenamientos);
+        labModificacion = (TextView)findViewById(R.id.labUpdate);
+
+        //Leemos los datos del usuario
+        ReadUserData();
     }
 
     @Override
@@ -24,5 +46,30 @@ public class PerfilActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Leemos los datos existentes del usuario
+    private void ReadUserData() {
+        try {
+            SharedPreferences preferences = getSharedPreferences("Logeo", Context.MODE_PRIVATE);
+
+            //Datos
+            String nombre = preferences.getString("USUARIO", null);
+            String contrasena = preferences.getString("CONTRASENA", null);
+            String modificacion = preferences.getString("MODIFICACION", null);
+
+            //Asignamos
+            tbNombre.setText(nombre);
+            tbContra.setText(contrasena);
+            if (modificacion == null) {
+                labModificacion.setText(labModificacion.getText() + " N/A");
+            }
+            else {
+                labModificacion.setText(labModificacion.getText() + " " + modificacion);
+            }
+        }
+        catch (Exception err) {
+            LogeoActivity.centralizarToast(getApplicationContext(), err.getMessage());
+        }
     }
 }
