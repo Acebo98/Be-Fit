908ae8ff5262575aa3e36be1370340ac5b879d07
sergio <sergio98.am30@gmail.com>
2019-04-11 13:59:42 +0200

    interfaz modificacion de pesos v1

---
+++ app/src/main/java/com/example/befit/PesosActivity.java
@@ -0,0 +1,27 @@
+package com.example.befit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class PesosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesos);
+
+        //Botón de ir atrás
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
