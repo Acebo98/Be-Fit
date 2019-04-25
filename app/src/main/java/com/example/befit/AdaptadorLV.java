c26937928705c4135c211de202dab3bb32eded3a
sergio <sergio98.am30@gmail.com>
2019-04-25 12:47:17 +0200

    buscador de entrenamientos hecho v2

--- app/src/main/java/com/example/befit/AdaptadorLV.java
+++ app/src/main/java/com/example/befit/AdaptadorLV.java
@@ -11,8 +11,9 @@ import java.util.ArrayList;
 
 public class AdaptadorLV extends BaseAdapter {
 
-    private Context context;
-    private ArrayList<VOSesion> lSesiones;
+    private Context context;                        //Contexto
+    private ArrayList<VOSesion> lSesiones;          //Datos que se van a mostrar (importante)
+    private ArrayList<VOSesion> lSesionesCopia;     //Copia de TODOS los datos de la BD. Útilizada para el buscador
 
     public AdaptadorLV(Context Context, ArrayList<VOSesion> Sesiones) {
         this.context = Context;
@@ -48,5 +49,26 @@ public class AdaptadorLV extends BaseAdapter {
         txtActu.setText(sesion.getActualizacion());
 
         return convertView;
+    }
+
+    //Filtrador
+    public void Filtrar(String nombre) throws Exception {
+        lSesiones.clear();                                          //Limpiamos la listview
+        lSesionesCopia = new DAOSesiones(context).ReadSesiones();   //Copia de la base de datos
+
+        //Si no hay texto cargamos todos los datos
+        if (nombre.trim().length() == 0) {
+            lSesiones.addAll(new DAOSesiones(context).ReadSesiones());
+        }
+        else {
+            for (VOSesion sesion : lSesionesCopia) {
+                if (sesion.getNombre().contains(nombre.trim()) == true) {
+                    lSesiones.add(sesion);
+                }
+            }
+        }
+
+        //Notificamos de los campos
+        this.notifyDataSetChanged();
     }
 }
