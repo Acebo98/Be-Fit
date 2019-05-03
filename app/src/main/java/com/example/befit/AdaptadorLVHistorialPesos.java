50e653c956ad31e94e2480f930912930c895afe2
sergio <sergio98.am30@gmail.com>
2019-05-03 08:39:20 +0200

    adaptador para la lista

---
+++ app/src/main/java/com/example/befit/AdaptadorLVHistorialPesos.java
@@ -0,0 +1,49 @@
+package com.example.befit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AdaptadorLVHistorialPesos extends BaseAdapter {
+
+    private Context context;                //Contexto
+    private ArrayList<VOPeso> lPesos;       //Lista de los pesos
+
+    public AdaptadorLVHistorialPesos(Context context, ArrayList<VOPeso> lPesos) {
+        this.context = context;
+        this.lPesos = lPesos;
+    }
+
+    @Override
+    public int getCount() {
+        return this.lPesos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.lPesos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        VOPeso peso = (VOPeso) getItem(position);
+        convertView = LayoutInflater.from(context).inflate(R.layout.item_registro_peso, null);
+
+        //Texto a mostrar
+        TextView tbFecha = (TextView)convertView.findViewById(R.id.tbRegistroPeso);
+        tbFecha.setText(peso.getFecha_Peso());
+
+        return convertView;
+    }
+}
