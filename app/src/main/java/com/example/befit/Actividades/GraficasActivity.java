880abdef840102f10d3d10897c0c6cdf5c1f78e6
sergio <sergio98.am30@gmail.com>
2019-05-14 16:32:20 +0200

    graficas wip

--- app/src/main/java/com/example/befit/Actividades/GraficasActivity.java
+++ app/src/main/java/com/example/befit/Actividades/GraficasActivity.java
@@ -1,17 +1,34 @@
 package com.example.befit.Actividades;
 
+import android.graphics.Color;
+import android.service.autofill.Dataset;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.example.befit.Dialogos.DialogoAlerta;
 import com.example.befit.Modelos.DAOTag;
 import com.example.befit.R;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 
 public class GraficasActivity extends AppCompatActivity {
 
@@ -47,6 +64,9 @@ public class GraficasActivity extends AppCompatActivity {
                 throw new Exception("error");
             }
             SacarArreglos(tagsSesiones);                        //Pasamos los datos a vectores
+
+            //Creamos las tablas
+            this.createCharts();
         }
         catch (Exception err) {
             DialogFragment dialogFragment = new DialogoAlerta();
@@ -81,5 +101,115 @@ public class GraficasActivity extends AppCompatActivity {
             nCuantos[i] = tagsSesiones.get(tag);
             i++;
         }
+    }
+
+    //Estructura de la tabla
+    private Chart getSameChart(Chart chart, String descripcion, int textColor, int background, int animateY) {
+        chart.getDescription().setText(descripcion);
+        chart.getDescription().setTextSize(15);
+        chart.setBackgroundColor(background);
+
+        chart.animateY(animateY);
+
+        return chart;
+    }
+
+    //Leyenda
+    private void legend(Chart chart) {
+        Legend legend = chart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+
+        ArrayList<LegendEntry> entries = new ArrayList<>();
+        for (int i = 0; i < entries.size(); i++) {
+            LegendEntry legendEntry = new LegendEntry();
+            legendEntry.formColor = Color.RED;
+            legendEntry.label = tags[i];
+            entries.add(legendEntry);
+        }
+        legend.setCustom(entries);
+    }
+
+    //Rellenamos las tablas
+    private ArrayList<BarEntry> getBarEntries() {
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < tags.length; i++) {
+            entries.add(new BarEntry(i, nCuantos[i]));
+        }
+
+        return entries;
+    }
+    private ArrayList<PieEntry> getPieEntries() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < tags.length; i++) {
+            entries.add(new PieEntry(nCuantos[i]));
+        }
+
+        return entries;
+    }
+
+    //Rellenamos el eje X con las etiquetas
+    private void axisX(XAxis axis) {
+        axis.setGranularityEnabled(true);
+        axis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        axis.setValueFormatter(new IndexAxisValueFormatter(tags));
+    }
+
+    //Ejes X
+    private void axisLeft(YAxis axis) {
+        axis.setSpaceTop(30);
+        axis.setAxisMinimum(0);
+    }
+    private void axisRight(YAxis axis) {
+        axis.setEnabled(false);
+    }
+
+    //Creamos las gráficas
+    public void createCharts() {
+        //Gráfica de tablas
+        barChart = (BarChart) getSameChart(barChart, "Etiquetas", Color.BLUE, Color.WHITE, 3000);
+        barChart.setDrawGridBackground(true);
+        barChart.setDrawBarShadow(true);
+        barChart.setData(getBarData());
+        barChart.invalidate();
+        axisX(barChart.getXAxis());
+        axisLeft(barChart.getAxisLeft());
+        axisRight(barChart.getAxisRight());
+
+        //Gráfica de sector
+        pieChart = (PieChart) getSameChart(pieChart, "Etiquetas", Color.GRAY, Color.MAGENTA, 3000);
+        pieChart.setHoleRadius(10);
+        pieChart.setTransparentCircleRadius(12);
+        pieChart.setData(getPieData());
+        pieChart.invalidate();
+        //pieChart.setDrawHoleEnabled(true);
+    }
+
+    //Datos a mostrar...
+    private DataSet getData(DataSet dataSet) {
+        //dataSet.setColor(int[] colores);
+        dataSet.setValueTextSize(Color.WHITE);
+        dataSet.setValueTextSize(10);
+
+        return dataSet;
+    }
+    private BarData getBarData() {
+        BarDataSet barDataSet = (BarDataSet) getData(new BarDataSet(getBarEntries(), ""));
+        barDataSet.setBarShadowColor(Color.GRAY);
+
+        BarData barData = new BarData(barDataSet);
+        barData.setBarWidth(0.45f);
+
+        return barData;
+    }
+    private PieData getPieData() {
+        PieDataSet pieDataSet = (PieDataSet) getData(new PieDataSet(getPieEntries(), ""));
+
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueFormatter(new PercentFormatter());
+
+        return new PieData(pieDataSet);
     }
 }
