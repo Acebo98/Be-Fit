7b9f6c88fe3e7d7ee029186479022d918ea94dc3
sergio <sergio98.am30@gmail.com>
2019-05-21 14:03:50 +0200

    actividad de feedback wip

--- app/src/main/java/com/example/befit/Actividades/FeedbackActivity.java
+++ app/src/main/java/com/example/befit/Actividades/FeedbackActivity.java
@@ -3,14 +3,24 @@ package com.example.befit.Actividades;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 
 import com.example.befit.R;
 
 public class FeedbackActivity extends AppCompatActivity {
 
-    
+    //Controles
+    EditText tbTitulo;
+    EditText tbMensaje;
+    Spinner spnTipo;
+    Button btnEnviar;
 
-    String[] lReportes;
+    String[] lReportes;                         //Arreglo con los tipos de reporte
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +31,29 @@ public class FeedbackActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //
+        //IDS
+        tbTitulo = (EditText) findViewById(R.id.tbTituloOpinion);
+        tbMensaje = (EditText) findViewById(R.id.tbCuerpoOpinion);
+        spnTipo = (Spinner) findViewById(R.id.spnTipoOpinion);
+        btnEnviar = (Button) findViewById(R.id.btnEnviarOpinion);
+
+        //Spinner
+        lReportes = new String[] {getString(R.string.opinion_bug), getString(R.string.opinion_dar)};
+        spnTipo.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, lReportes));
+
+        //Evento de enviar
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ComprobarCamposRellenos() == true) {
+
+                }
+                else {
+                    LogeoActivity.centralizarToast(getApplicationContext(), getString(R.string.campos_opiniones));
+                }
+            }
+        });
     }
 
     @Override
@@ -31,5 +63,10 @@ public class FeedbackActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Comprobamos que los campos estén rellenos
+    private boolean ComprobarCamposRellenos() {
+        return tbMensaje.getText().toString().trim().length() > 0 && tbTitulo.getText().toString().trim().length() > 0;
     }
 }
